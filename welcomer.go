46cf99efa23b0df4ec580870551284436e528847
Paweł Litwinowicz <litwinowiczpawel@gmail.com>
2020-03-22 20:28:50 +0100

    Extract makePrinter
    No functional changes.

--- welcomer.go
+++ welcomer.go
@@ -1,7 +1,13 @@
 package main
 
+import "fmt"
+
 func makeWelcomer() func() {
+	return makePrinter("Elo Ziemio!")
+}
+
+func makePrinter(data string) func() {
 	return func() {
-		fmt.Println("Elo Ziemio!")
+		fmt.Println(data)
 	}
 }
