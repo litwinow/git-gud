2d29eefc93caf962f0260cf281dfbc92aa8f4437
Paweł Litwinowicz <priviet@MacBook-Pro-Pawel-2.local>
2020-03-22 20:21:31 +0100

    Extract makeWelcomer
    No functional changes.

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func makeWelcomer() func() {
+	return func() {
+		fmt.Println("Elo Ziemio!")
+	}
+}
+
 func main() {
-	fmt.Println("Elo Ziemio!")
+	welcomer := makeWelcomer()
+	welcomer()
 }
