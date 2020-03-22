83d79a5900a77b1eb38c457ac2ae57daea213e52
Paweł Litwinowicz <litwinowiczpawel@gmail.com>
2020-03-22 21:01:29 +0100

    inline makePrinter

--- welcomer.go
+++ welcomer.go
@@ -1,10 +1,6 @@
 package main
 
 func makeWelcomer() func() {
-	return makePrinter("Elo Ziemio!")
-}
-
-func makePrinter(data string) func() {
 	sink, _ := makeMemorySink()
-	return func() { sink(data) }
+	return func() { sink("Elo Ziemio!") }
 }
